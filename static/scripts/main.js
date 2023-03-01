79cb9aeab087c5699e058505592ff319ea878d00
jemclift <jemclift@protonmail.com>
2023-03-01 21:36:17 +0000

    mostly done main ui

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -30,6 +30,9 @@ function timeSince(date) {
     let value = Math.floor(interval)
     let unit = units[index]
 
+    if (isNaN(value)) { value = 0 }
+    if (unit == undefined) { unit = 'second'}
+
     // plural units
     if ( value > 1 ) { unit += 's' }
 
@@ -45,4 +48,64 @@ function formatAllDates() {
         let post_date = Date.parse(date_string)
         element.innerHTML = "posted "+timeSince(post_date)+" ago"
     }
+}
+
+function mobileCheck() {
+    let isMobile = document.getElementById("lets-go-mobile")
+
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    
+    // console.log(w/h + " " + isMobile.value)
+
+    if (w/h < 0.75 && isMobile.value == 0) {
+        // go mobile!
+
+        let like_buttons = document.getElementsByClassName("like")
+        let dislike_buttons = document.getElementsByClassName("dislike")
+        let post_dates = document.getElementsByClassName("post-date")
+        let post_scores = document.getElementsByClassName("post-score")
+
+        for (like_button of like_buttons) {
+            like_button.innerHTML = "👍" 
+        }
+        for (dislike_button of dislike_buttons) {
+            dislike_button.innerHTML = "👎" 
+        }
+        for (post_date of post_dates) {
+            post_date.innerHTML = post_date.innerHTML.slice(7, -4)
+        }
+        for (post_score of post_scores) {
+            post_score.innerHTML = "s. scr "+post_score.innerHTML.slice(20)
+        }
+
+        isMobile.value = 1
+
+    } else if (w/h > 0.75 && isMobile.value == 1) {
+        // revert
+
+        let like_buttons = document.getElementsByClassName("like")
+        let dislike_buttons = document.getElementsByClassName("dislike")
+        let post_dates = document.getElementsByClassName("post-date")
+        let post_scores = document.getElementsByClassName("post-score")
+
+        for (like_button of like_buttons) {
+            like_button.innerHTML = "👍 Like"
+        }
+        for (dislike_button of dislike_buttons) {
+            dislike_button.innerHTML = "👎 Dislike"
+        }
+        for (post_date of post_dates) {
+            post_date.innerHTML = "posted "+post_date.innerHTML+" ago"
+        }
+        for (post_score of post_scores) {
+            post_score.innerHTML = "sustainability score "+post_score.innerHTML.slice(7)
+        }
+
+        isMobile.value = 0
+    }
+}
+
+if(window.addEventListener) {
+    window.addEventListener('resize', mobileCheck, true);
 }
